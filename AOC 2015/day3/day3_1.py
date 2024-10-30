43cca9d32b9ce0507154042bd570c25c8b3e8f60
lokisten <pankajkarki989@gmail.com>
2024-10-30 20:39:17 +0545

    day3

---
+++ AOC 2015/day3/day3_1.py
@@ -0,0 +1,21 @@
+# Open and read the directions from the input file
+with open('C:\\Users\\Acer\\Desktop\\AOC\\AOC 2015\\day3\\input_3.txt', 'r') as f:
+    directions = f.read()
+
+# Initialize history with the starting position
+history = [(0, 0)]
+
+# Iterate over each direction in the directions input
+for direction in directions:
+    last = history[-1]
+    if direction == 'v':
+        history.append((last[0], last[1] - 1))
+    elif direction == '^':
+        history.append((last[0], last[1] + 1))
+    elif direction == '>':
+        history.append((last[0] + 1, last[1]))
+    elif direction == '<':
+        history.append((last[0] - 1, last[1]))
+
+# Count the unique positions visited
+print(len(set(history)))
